1a3d4ec7df4627c5fd384b0d18097a3cb3aa58e3
trainingsrepo <trainings.anil@gmail.com>
2025-11-10 19:58:01 +0530

    feature101 changes in application.java file by vivek

--- Application.java
+++ Application.java
@@ -16,4 +16,21 @@ class AnotherMessage {
     static String generateAnotherMessage() {
         return "Here is another message";
     }
+	#feature101 code changes by vivek in oct 2025
+	public class ScannerExample {
+
+    public static void main(String... args) {
+        String wordsAndNumbers = """
+                Longing rusted furnace
+                daybreak 17 benign 
+                9 homecoming 1 
+                freight car
+                """;
+
+        try (Scanner scanner = new Scanner(wordsAndNumbers)) {
+            scanner.findAll("benign").map(MatchResult::group).forEach(IO::println);
+        }
+    }
+}
+#end of feature101
 }
