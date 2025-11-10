21faec21bae13627bb640a553acd9e643739e290
trainingsrepo <trainings.anil@gmail.com>
2025-11-10 20:03:20 +0530

    feature102 changes in Application.java file by ethan

--- Application.java
+++ Application.java
@@ -16,4 +16,25 @@ class AnotherMessage {
     static String generateAnotherMessage() {
         return "Here is another message";
     }
+	#feature102 code changes by Ethan in Oct 2025
+	public class MultipleClassesInSameFile {
+    public static void main(String[] args) {
+ 
+        IO.println(GenerateMessage.generateMessage());
+        IO.println(AnotherMessage.generateAnotherMessage());
+    }
+}
+ 
+class GenerateMessage {
+    static String generateMessage() {
+        return "Here is one message";
+    }
+}
+ 
+class AnotherMessage {
+    static String generateAnotherMessage() {
+        return "Here is another message";
+    }
+}
+#end of feature102 code changes
 }
